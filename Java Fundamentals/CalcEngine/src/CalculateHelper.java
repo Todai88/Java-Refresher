5eeb3f4d737b7e89a3f0d750d33cb3176268c3d2
Joakim Kakaei <joabaj88@gmail.com>
2018-11-26 17:11:58 +0000

    Added a calculator helper

---
+++ Java Fundamentals/CalcEngine/src/CalculateHelper.java
@@ -0,0 +1,48 @@
+public class CalculateHelper {
+    MathCommand command;
+    double leftValue;
+    double rightValue;
+    double result;
+
+    public void process(String statement) {
+        String[] parts = statement.split(" ");
+        String commandString = parts[0];
+        leftValue = Double.parseDouble(parts[1]);
+        rightValue = Double.parseDouble(parts[2]);
+
+        setCommandFromString(commandString);
+
+        CalculateBase calculator = null;
+        switch(command) {
+            case Add:
+                calculator = new Adder(leftValue, rightValue);
+                break;
+            case Subtract:
+                calculator = new Subtractor(leftValue, rightValue);
+                break;
+            case Divide:
+                calculator = new Divider(leftValue, rightValue);
+                break;
+            case Multiply:
+                calculator = new Multiplier(leftValue, rightValue);
+                break;
+        }
+        calculator.calculate();
+        System.out.print("Result: ");
+        System.out.println(calculator.getResult());
+    }
+
+    private void setCommandFromString(String commandString) {
+        if(commandString.equalsIgnoreCase(MathCommand.Add.toString())) {
+            command = MathCommand.Add;
+        } else if(commandString.equalsIgnoreCase(MathCommand.Subtract.toString())) {
+            command = MathCommand.Subtract;
+        } else if(commandString.equalsIgnoreCase(MathCommand.Divide.toString())) {
+            command = MathCommand.Divide;
+        } else  if(commandString.equalsIgnoreCase(MathCommand.Multiply.toString())) {
+            command = MathCommand.Multiply;
+        } else {
+            command = MathCommand.Add;
+        }
+    }
+}
--- Java Fundamentals/CalcEngine/src/Main.java
+++ Java Fundamentals/CalcEngine/src/Main.java
@@ -1,16 +1,26 @@
 public class Main {
 
     public static void main(String[] args){
-        MathEquation[] equations = new MathEquation[4];
-        equations[0] = new MathEquation(100.0, 50.0, 'd');
-        equations[1] = new MathEquation(25.0, 92.0, 'a');
-        equations[2] = new MathEquation(225.0, 17.0, 's');
-        equations[3] = new MathEquation(11.0, 3.0, 'm');
+        useMathEquation();
+        useCalculatorBase();
+        useCalculatorHelper();
+    }
 
-        for (MathEquation equation: equations) {
-            System.out.println(equation.execute());
+    private static void useCalculatorHelper() {
+        String[] statements = {
+                "divide 100.0 50.0",
+                "add 25.0 92.0",
+                "subtract 225.0 17.0",
+                "multiply 11.0 3.0"
+        };
+
+        CalculateHelper calcHelper = new CalculateHelper();
+        for(String statement:statements) {
+            calcHelper.process(statement);
         }
+    }
 
+    private static void useCalculatorBase() {
         System.out.println("Using inheritance >> ");
         CalculateBase[] calculators = {
                 new Divider(100.0d, 50.0d),
@@ -24,9 +34,20 @@ public class Main {
         }
     }
 
+    private static void useMathEquation() {
+        MathEquation[] equations = new MathEquation[4];
+        equations[0] = new MathEquation(100.0, 50.0, 'd');
+        equations[1] = new MathEquation(25.0, 92.0, 'a');
+        equations[2] = new MathEquation(225.0, 17.0, 's');
+        equations[3] = new MathEquation(11.0, 3.0, 'm');
+
+        for (MathEquation equation: equations) {
+            System.out.println(equation.execute());
+        }
+    }
+
     public static MathEquation create(double leftVal, double rightVal, char opCode) {
         MathEquation equation = new MathEquation(leftVal, rightVal, opCode);
-
         return equation;
     }
 }
